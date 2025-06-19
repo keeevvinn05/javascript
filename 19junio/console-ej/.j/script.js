86d28c8131e067b49276e466840ca602e343578b
keeevvinn05 <barrosok526@gmail.com>
2025-06-19 10:42:48 +0200

    guardar nuevo

---
+++ 19junio/console-ej/.j/script.js
@@ -0,0 +1,47 @@
+// //1. saludo personalizado
+// //define la funcion saludr
+// //parametro = nombre
+// function Saludar(kevin){
+// return `¡¡hola, ${kevin}!!`;
+// }
+
+// //prueba en consola:
+// console.log(Saludar('kevin'));
+
+//2. funcion suma de dos numeros
+//definir la funcion sumar
+
+// function sumar(a , b){
+//    return  a + b
+// }
+  
+
+// console.log(sumar(3 , 7));
+
+//3. verificar par o impar
+//define la funcion esPar
+// function esPar (numero){
+//   return numero %2===0
+// }
+// console.log(esPar(4));
+// console.log(esPar(9));
+
+//3. longitud de cadena 
+//define la funcion longitud
+
+// function longitud(cadena){
+//   return cadena.length;
+// }
+// console.log(longitud('esternocleidomastoideo'));
+// console.log(longitud('hola yo soy kevin'));
+
+//4. repetir un texto
+// function repetir (texto, veces){
+//    let resultado = '';
+//    for (let i = 0; i < veces;i++){
+//     resultado += texto;
+//    }
+//    return resultado
+//    }
+//  console.log(repetir('kevin', 999));
+
